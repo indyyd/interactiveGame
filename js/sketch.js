e69d3780e80efb493097e47e76b584039c4337d8
Indy <116882977+indyyd@users.noreply.github.com>
2022-11-15 10:14:47 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -4,16 +4,48 @@ var heightRect = 50;
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
-    background(220);
+    background(0);
 }
 
+//you need to have at least 1 draw function for it to work properly
 function draw() {
 
     
 }
 
+//the diff. between mousePressed and mouseClicked is that mouseClicked only applies to the left mouse button
 function mousePressed () {
-    rectMode(CENTER);
-    fill(255, 255, 255);
-    rect(mouseX, mouseY, widthRect, heightRect);
+    
+    if(mouseButton == LEFT) {
+        rectMode(CENTER);
+        fill(255, 255, 255);
+        rect(mouseX, mouseY, widthRect, heightRect);
+    }
+
+    if(mouseButton == RIGHT) {
+        ellipseMode(CENTER);
+        fill(255, 255, 255);
+        ellipse(mouseX, mouseY, widthRect, widthRect);
+    }
+    
+    
+}
+
+function keyPressed () {
+    if(keyCode == ENTER) {
+        background(0);
+    }
+
+    if(keyCode == UP_ARROW) {
+        rect(mouseX, mouseY, widthRect+20, heightRect+40)
+    }
+
+    if(keyCode == DOWN_ARROW) {
+        ellipse(mouseX, mouseY, widthRect+40, heightRect-20)
+    }
+
+    if(keyCode == LEFT_ARROW) {
+        fill(random(0,255), random(0, 255), random(0, 255))
+        triangle(mouseX, mouseY, mouseX=50, mouseY, mouseX=20, mouseY=50)
+    }
 }
