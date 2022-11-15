39675c024ceb2636f3ae7f46daed0a6fe41beff2
Indy <116882977+indyyd@users.noreply.github.com>
2022-11-15 09:35:42 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,19 @@
+var widthRect = 50;
+var heightRect = 50;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    background(220);
 }
 
 function draw() {
-    background(220);
 
+    
+}
+
+function mousePressed () {
+    rectMode(CENTER);
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    rect(mouseX, mouseY, widthRect, heightRect);
 }
